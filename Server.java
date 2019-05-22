810cb7e3f690e28053a2e9de06ab7f135d31ad21
Unknown <migud.carvalho@gmail.com>
2019-05-22 11:29:41 +0100

    Servers

---
+++ Server.java
@@ -0,0 +1,32 @@
+
+public class Server {
+
+	private InetAddress address
+	private port
+
+	public Server ( String ipAddress ) {
+
+		address = InetAddress.getByName( ipAddress );
+
+	}
+
+	// ip
+	// tamanho do cluster
+	public void main ( String args[] ) {
+
+   		if( args.length != 2 ) {
+
+   	 		System.out.println( "WRONG No OF ARGUMENTS" );
+   	 		return;
+
+   	 	}
+
+        Server server = new Server( args[0], args[1] );
+
+        Thread listener = new Thread( new ServerListener() );
+
+        listener.start();
+
+	}
+
+}
---
+++ ServerListener.java
@@ -0,0 +1,20 @@
+
+public class ServerListener implements Runable () {
+
+	public ServerListener () {
+
+
+
+	}
+
+	public void run () {
+
+		while(1){
+
+			
+
+		}
+
+	}
+
+}
