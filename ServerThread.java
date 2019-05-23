ac230a01f528b19a23ff04ceadd45773441fdec1
unknown <gomespedromiguel@hotmail.com>
2019-05-23 18:37:14 +0100

    connection between client and server using ssl

---
+++ ServerThread.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class ServerThread extends Thread{
+
+	Socket socket;
+	
+	ServerThread(Socket socket) {
+		this.socket=socket;
+	}
+	
+	public void run() {
+		try {
+			PrintWriter printWriter = new PrintWriter(socket.getOutputStream(),true);
+			BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			System.out.println("user " + bufferedReader.readLine() + " is now connected to the server.\n");
+			while (true) {
+				printWriter.println(bufferedReader.readLine() + " echo");
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
