5364bfc16bb084400efac95e9c3ae6c09781cde3
Unknown <migud.carvalho@gmail.com>
2019-05-25 18:34:31 +0100

    server cenas

--- ServerListener.java
+++ ServerListener.java
@@ -1,19 +1,47 @@
+import java.net.*;
+import java.io.*;
 
-public class ServerListener implements Runable () {
+public class ServerListener implements Runnable {
 
-	public ServerListener () {
+	private ServerSocket socket;
 
+	public ServerListener (int port, InetAddress address) {
 
+		try {socket = new ServerSocket(port, 50, address);} catch (IOException e){ e.printStackTrace();}
 
 	}
 
 	public void run () {
 
-		while(1){
+		try {
 
-			
+			while(true) {
 
+				System.out.println("Server listening...");
+
+				Socket connectionSocket = socket.accept();
+				BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+				DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
+				String receivedmessage = inFromClient.readLine();
+				System.out.println("Received: " + receivedmessage);
+				//capitalizedSentence = clientSentence.toUpperCase() + 'n';
+				outToClient.writeBytes(receivedmessage);
+
+				long start = System.nanoTime();    
+				handledMessage(receivedmessage);
+				System.out.println(System.nanoTime() - start);
+
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
+
+	}
+
+	private void handledMessage(String receivedmessage) {
+
+		
 
 	}
 
